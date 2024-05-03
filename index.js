1639ec216674c97baf60a2e36b3d57892ab808bf
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-05-03 09:25:42 -0700

    update | fix?!??!

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { callbackify } = require('util');
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-  res.render("chat.ejs")
+  res.render("chat")
 });
 
 const replacements = {
